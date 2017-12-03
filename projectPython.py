288d031a280c4c707f6e1276a81c84b7a4de6a55
alibaltschun <alibaltschun@gmail.com>
2017-12-03 12:13:59 +0700

    project python and R

---
+++ projectPython.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  3 12:04:26 2017
+
+@author: Ali Baltschun
+"""
+
+# Data Processing
+
+# importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
