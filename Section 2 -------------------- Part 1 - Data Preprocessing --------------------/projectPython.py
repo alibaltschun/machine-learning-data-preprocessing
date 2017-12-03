5506dc523e179dd51dbe42caa7d6d8e6d45ec6c0
alibaltschun <alibaltschun@gmail.com>
2017-12-03 15:11:44 +0700

    Splitting the dataset into the Train set and Test set

--- Section 2 -------------------- Part 1 - Data Preprocessing --------------------/projectPython.py
+++ Section 2 -------------------- Part 1 - Data Preprocessing --------------------/projectPython.py
@@ -22,3 +22,46 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values= 'NaN' , strategy= 'mean' , axis=0)
 imputer = imputer.fit(x[:,1:3])
 x[:,1:3] = imputer.transform(x[:,1:3])
+
+# Encoding categorial data
+from sklearn.preprocessing import LabelEncoder , OneHotEncoder
+labelEncoder_X = LabelEncoder()
+x[:,0] = labelEncoder_X.fit_transform(x[:,0]) 
+
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+x = oneHotEncoder.fit_transform(x).toarray()
+
+labelEncoder_Y = LabelEncoder()
+y = labelEncoder_Y.fit_transform(y) 
+
+# Splitting the dataset into Trainsing set and Test set
+from sklearn.cross_validation import train_test_split
+x_train , x_test , y_train, y_test = train_test_split(x,y ,test_size = 0.2, random_state = 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
